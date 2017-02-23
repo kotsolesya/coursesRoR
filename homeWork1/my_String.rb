ff9f4ac7edabca35071c734c563c33ccef1abb8b
Kots Olesya <kots.olesya@gmail.com>
2017-02-23 19:21:33 +0200

    add regular expression

--- homeWork1/my_String.rb
+++ homeWork1/my_String.rb
@@ -8,15 +8,27 @@ class String
       elsif stack.pop == brackets[c]
       else return false
       end
-      end
+    end
     stack.empty?
+  end
+
+  def valid_string?
+    strim = self.gsub(/[^\[\]\(\)\{\}\<>]/,'')
+    return true if strim.empty?
+    return false if strim.size.odd?
+    loop do
+      s = strim.gsub('()', '').gsub('[]', '').gsub('{}', '').gsub('<>', '')
+      return true if s.empty?
+      return false if s == strim
+      strim = s
+    end
   end
 
   def valid_from_google?
     stack = []
     each_char do |char|
       case char
-      when '{', '[', '('
+      when '{', '[', '(', '<'
         stack.push(char)
       when '}'
         x = stack.pop
@@ -27,6 +39,9 @@ class String
       when ')'
         x = stack.pop
         return false if x != '('
+      when '>'
+        x = stack.pop
+        return false if x != '<'
       end
     end
     stack.empty?
--- homeWork1/test.rb
+++ homeWork1/test.rb
@@ -2,21 +2,28 @@ require 'benchmark'
 require_relative 'my_String'
 class Test
   def test
-    str = '[][][][][][][][][]()()()()()()()()()(){}{}{}{}{}{}{}{}{}lola[[]][[]][[]]{{()}}['
+    str = '[][][][][][][][][]()()()()()()()()()(){}{}{}{}{}{}{}{}{}lola[[]][[]][[]]{{()}}[<><><><><<><><><><><><><><><><><><><><><>'
     p str.valid?
     p 'lola[[]][[]][[]]{{()}}'.valid?
-    p '{(})'.valid?
+    p '{(})<>'.valid?
     p '{{[]'.valid?
-    p '{[()]}'.valid?
+    p '{[()]}<>'.valid?
+    p '======================'
+    p str.valid_string?
+    p 'lola[[]][[]][[]]{{()}}'.valid_string?
+    p '{(})<>'.valid_string?
+    p '{{[]'.valid_string?
+    p '{[()]}<>'.valid_string?
     p '======================'
     p str.valid_from_google?
     p 'lola[[]][[]][[]]{{()}}'.valid_from_google?
-    p '{(})'.valid_from_google?
+    p '{(})<>'.valid_from_google?
     p '{{[]'.valid_from_google?
-    p '{[()]}'.valid_from_google?
+    p '{[()]}<>'.valid_from_google?
     Benchmark.bm do |x|
-      x.report("valid my\t") { str.valid? }
-      x.report('valid google') { str.valid_from_google? }
+      x.report("valid my stack\t") { str.valid? }
+      x.report("valid regex\t") { str.valid_string? }
+      x.report("valid google\t") { str.valid_from_google? }
     end
   end
 end
